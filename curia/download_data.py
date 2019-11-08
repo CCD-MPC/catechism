29f8ea26fe6f1492bef70c6afb9d6e2fb7433c46
ben <bengetch@gmail.com>
2019-11-07 21:12:34 -0500

    decrypt file after pulling

--- curia/download_data.py
+++ curia/download_data.py
@@ -4,6 +4,7 @@ sys.path.append("/app/curia/")
 
 from curia.swift import SwiftData
 from curia.dataverse import DataverseData
+from curia.crypto import Dec
 
 
 def download_swift_data(c):
@@ -15,8 +16,7 @@ def download_swift_data(c):
     container = swift_cfg['data']['container_name']
     file = swift_cfg['data']['file_name']
 
-    fname = ".".join(file.split(".")[:-1])
-    pname = ".".join([fname, "json"])
+    pname = "{}.json".format(file)
 
     swift_data = SwiftData(swift_cfg)
     swift_data.get_data(container, file, "/data/", file)
@@ -35,8 +35,7 @@ def download_dataverse_data(c):
     dv_cfg = c["dataverse"]["source"]
 
     file = dv_cfg['data']['file_name']
-    fname = ".".join(file.split(".")[:-1])
-    pname = ".".join([fname, "json"])
+    pname = "{}.json".format(file)
 
     dv_data = DataverseData(dv_cfg)
     dv_data.get_data(file, "/data/", file)
@@ -48,6 +47,11 @@ if __name__ == "__main__":
     conf = open("/data/conf.json", 'r').read()
     cfg_json = json.loads(conf)
 
+    enc_k = open("/data/enc-conf/k", 'r').read()
+    enc_nonce = open("/data/enc-conf/nonce", 'r').read()
+
+    enc_dict = {"k": enc_k, "nonce": enc_nonce}
+
     if cfg_json["backends"]["data"] == "dataverse":
 
         dv_host = open("/etc/dataverse-auth/dv_host").read()
@@ -56,6 +60,10 @@ if __name__ == "__main__":
         cfg_json["dataverse"]["source"]["auth"]["token"] = dv_token
 
         download_dataverse_data(cfg_json)
+
+        filename = "/data/{}".format(cfg_json["dataverse"]["data"]["file_name"])
+        d = Dec(filename, enc_dict)
+        d.dec_and_write("{}.csv".format(filename))
 
     elif cfg_json["backends"]["data"] == "swift":
         auth_url = open("/etc/swift-auth/auth_url").read()
@@ -67,5 +75,9 @@ if __name__ == "__main__":
 
         download_swift_data(cfg_json)
 
+        filename = "/data/{}".format(cfg_json["swift"]["data"]["file_name"])
+        d = Dec(filename, enc_dict)
+        d.dec_and_write("{}.csv".format(filename))
+
     else:
-        raise Exception("Backend not recognized: {} \n".format(c["backends"]["data"]))
+        raise Exception("Backend not recognized: {} \n".format(cfg_json["backends"]["data"]))
